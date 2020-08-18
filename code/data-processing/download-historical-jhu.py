2e8dbdc7f42bcc57e956fb5e22e97edaf9785e3f
elray1 <evan.l.ray@gmail.com>
2020-08-18 13:20:38 -0700

    clean up file download, resolves issue #10

--- code/data-processing/download-historical-jhu.py
+++ code/data-processing/download-historical-jhu.py
@@ -2,11 +2,18 @@ import pandas as pd
 import requests
 import json
 import os
+import time
 
+# create directory to save files if it doesn't already exist
+if (not os.path.isdir('../../data-raw/JHU/')):
+    os.mkdir('../../data-raw/JHU/')
+
+# which files we will download
 base_files = [
     'time_series_covid19_deaths_US.csv',
     'time_series_covid19_confirmed_US.csv'
 ]
+
 
 for base_file in base_files:
     # retrieve information about all commits that modified the file we want
@@ -37,7 +44,21 @@ for base_file in base_files:
     # sha for the last commit made each day
     commit_shas_to_get = {}
     for index, commit_date in enumerate(commit_dates):
+        # location in which to save file
         result_path =  '../../data-raw/JHU/' + commit_date + '_' + base_file
+
+        # delete file if it was downloaded on the commit date since it may not
+        # be the last commit that day
+        if os.path.isfile(result_path):
+            creation_time = os.path.getctime(result_path)
+            creation_date = time.strftime(
+                "%Y-%m-%d",
+                time.gmtime(creation_time)
+            )
+            if creation_date == commit_date:
+                os.remove(result_path)
+
+        # record as a sha to download if applicable
         if (commit_date not in commit_shas_to_get) and (not os.path.isfile(result_path)):
             commit_shas_to_get[commit_date] = all_commits[index]['sha']
     
