8210544cafc5e239f28e76807a32be7ca8ec4f49
Evan Ray <elray1@users.noreply.github.com>
2020-12-28 09:12:57 -0500

    Update code/data-processing/download-historical-healthdata.R

--- code/data-processing/download-historical-healthdata.R
+++ code/data-processing/download-historical-healthdata.R
@@ -202,7 +202,7 @@ for (i in seq_len(nrow(timeseries_revisions_meta))) {
   if (!file.exists(destination_path)) {
     data <- suppressMessages(readr::read_csv(
       timeseries_revisions_meta$file_link[i]%>%
-        httr::GET(config = httr::config(ssl_verifypeer = FALSE))%>% 
+        httr::GET(config = httr::config(ssl_verifypeer = FALSE)) %>% 
         content(as="text"),
       col_types = cols_only(
         state = col_character(),
