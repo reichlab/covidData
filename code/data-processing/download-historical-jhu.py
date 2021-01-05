6fd9e215ca9612d13aabbc92c6de75cf4a2d889a
Evan Ray <elray@umass.edu>
2021-01-05 09:31:31 -0500

    reference correct file when identifying commits for jhu data

--- code/data-processing/download-historical-jhu.py
+++ code/data-processing/download-historical-jhu.py
@@ -18,14 +18,14 @@ base_files = [
 for base_file in base_files:
     # retrieve information about all commits that modified the file we want
     all_commits = []
-    
+
     page = 0
     while True:
         page += 1
         r = requests.get(
             'https://api.github.com/repos/CSSEGISandData/COVID-19/commits',
             params = {
-                'path': 'csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv',
+                'path': 'csse_covid_19_data/csse_covid_19_time_series/' + base_file,
                 'page': str(page)
             }
         )
@@ -34,13 +34,13 @@ for base_file in base_files:
             break
         
         all_commits += json.loads(r.text or r.content)
-    
-    
+
+
     # date of each commit
     commit_dates = [
         commit['commit']['author']['date'][0:10] for commit in all_commits
     ]
-    
+
     # sha for the last commit made each day
     commit_shas_to_get = {}
     for index, commit_date in enumerate(commit_dates):
